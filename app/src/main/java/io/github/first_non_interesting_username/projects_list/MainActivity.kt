af35eaa1b850fbec46accd9395a19fcd858ec0bf
First-Non-Interesting-Username <janekmusin@proton.me>
2026-07-27 16:59:27 +0200

    let's make a deal. I write useless commits and you don't care. okay?

---
+++ app/src/main/java/io/github/first_non_interesting_username/projects_list/MainActivity.kt
@@ -0,0 +1,55 @@
+package io.github.first_non_interesting_username.projects_list
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import io.github.first_non_interesting_username.projects_list.ui.theme.ProjectsTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ProjectsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = Color.Cyan) {
+        Text(
+            text = "Hi, my name is $name!",
+            modifier = modifier.padding(24.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ProjectsTheme {
+        Greeting("Android")
+    }
+}
