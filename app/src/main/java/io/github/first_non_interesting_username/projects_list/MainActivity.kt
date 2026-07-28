ea9e619c0d36e738df69d5fe624290d34e3b16e3
First-Non-Interesting-Username <janekmusin@proton.me>
2026-07-28 17:43:45 +0200

    add top bar

--- app/src/main/java/io/github/first_non_interesting_username/projects_list/MainActivity.kt
+++ app/src/main/java/io/github/first_non_interesting_username/projects_list/MainActivity.kt
@@ -5,20 +5,29 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateMapOf
@@ -38,98 +47,135 @@ class MainActivity : ComponentActivity() {
         setContent {
             ProjectsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MyColumn(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding)
-                    )
+                    Scaffold(
+                        modifier = Modifier.fillMaxSize(),
+                        topBar = { AppTopBar() },
+                    ) { innerPadding ->
+                        MyColumn(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(innerPadding)
+                        )
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun TaskInputField(
-    task: String,
-    onTaskChange: (String) -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    OutlinedTextField(
-        value = task,
-        onValueChange = onTaskChange,
-        label = { Text("Task") },
-        singleLine = true,
-        modifier = modifier,
-    )
-}
 
-@Composable
-fun TaskDisplay(
-    task: String,
-    taskNumber: String,
-    modifier: Modifier = Modifier,
-) {
-    Surface(
-        shape = RoundedCornerShape(12.dp),
-        color = MaterialTheme.colorScheme.surfaceVariant,
-        modifier = modifier,
+    @Composable
+    fun TaskInputField(
+        task: String,
+        onTaskChange: (String) -> Unit,
+        modifier: Modifier = Modifier,
     ) {
-        Text(
-            text = "Task $taskNumber: $task",
-            style = MaterialTheme.typography.bodyLarge,
-            modifier = Modifier.padding(16.dp)
+        OutlinedTextField(
+            value = task,
+            onValueChange = onTaskChange,
+            label = { Text("Task") },
+            singleLine = true,
+            modifier = modifier,
         )
     }
-}
 
-@Composable
-fun ResetButton(
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    Button(onClick = onClick, modifier = modifier) {
-        Text("Reset")
-    }
-}
-
-@Composable
-fun ConfirmationButton(onClick: () -> Unit) {
-    FilledTonalButton(onClick = { onClick() }) {
-        Text("Add task")
-    }
-}
-
-@Composable
-fun MyColumn(modifier: Modifier = Modifier) {
-    var taskList = remember { mutableStateMapOf<Int, String>() }
-    var nextId by remember { mutableStateOf(0) }
-
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
+    @Composable
+    fun TaskDisplay(
+        task: String,
+        taskNumber: String,
+        modifier: Modifier = Modifier,
     ) {
-        TaskInputField(
-            task = taskList.getOrPut(nextId) { "" },
-            onTaskChange = { taskList[nextId] = it },
-        )
-        Spacer(Modifier.height(8.dp))
-        ConfirmationButton(
-            onClick = ( {nextId++ })
-        )
-        for ((taskIndex, task) in taskList) {
-            if (task.isNotEmpty() && taskIndex != nextId) {
-                Spacer(Modifier.height(8.dp))
-                TaskDisplay(task = task, taskNumber = taskIndex.toString()
-                )
-            }
+        Surface(
+            shape = RoundedCornerShape(12.dp),
+            color = MaterialTheme.colorScheme.surfaceVariant,
+            modifier = modifier,
+        ) {
+            Text(
+                text = "Task $taskNumber: $task",
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.padding(16.dp)
+            )
         }
-        Spacer(Modifier.height(8.dp))
-        ResetButton(onClick = {
+    }
+
+    @Composable
+    fun ResetButton(
+        onClick: () -> Unit,
+        modifier: Modifier = Modifier,
+    ) {
+        Button(onClick = onClick, modifier = modifier) {
+            Text("Reset")
+        }
+    }
+
+    @Composable
+    fun ConfirmationButton(onClick: () -> Unit) {
+        FilledTonalButton(onClick = { onClick() }) {
+            Text("Add task")
+        }
+    }
+
+    @Composable
+    fun MyColumn(modifier: Modifier = Modifier) {
+        var taskList = remember { mutableStateMapOf<Int, String>() }
+        var nextId by remember { mutableStateOf(0) }
+
+        Column(
+            modifier = modifier,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            TaskInputField(
+                task = taskList.getOrPut(nextId) { "" },
+                onTaskChange = { taskList[nextId] = it },
+            )
+            Spacer(Modifier.height(8.dp))
+            ConfirmationButton(
+                onClick = ({ nextId++ })
+            )
             for ((taskIndex, task) in taskList) {
-                taskList[taskIndex] = ""
+                if (task.isNotEmpty() && taskIndex != nextId) {
+                    Spacer(Modifier.height(8.dp))
+                    TaskDisplay(
+                        task = task, taskNumber = taskIndex.toString()
+                    )
+                }
             }
-        })
+            Spacer(Modifier.height(8.dp))
+            ResetButton(onClick = {
+                for ((taskIndex, task) in taskList) {
+                    taskList[taskIndex] = ""
+                }
+            })
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun AppTopBar() {
+        var expanded by remember { mutableStateOf(false) }
+
+        TopAppBar(
+            title = { Text("Projects List") },
+            modifier = Modifier,
+            actions = {
+                IconButton(onClick = { expanded = !expanded }) {
+                    Icon(Icons.Default.MoreVert, contentDescription = "More options")
+                }
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false }
+                ) {
+                    DropdownMenuItem(
+                        text = { Text("Option 1") },
+                        onClick = { expanded = false }
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Option 2") },
+                        onClick = { expanded = false }
+                    )
+                }
+            }
+        )
     }
 }
+
