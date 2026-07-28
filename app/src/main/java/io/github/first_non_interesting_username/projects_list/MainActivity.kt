c7fef60f60b0366bec308ac4523655a37031f61b
First-Non-Interesting-Username <janekmusin@proton.me>
2026-07-28 11:34:15 +0200

    center text

--- app/src/main/java/io/github/first_non_interesting_username/projects_list/MainActivity.kt
+++ app/src/main/java/io/github/first_non_interesting_username/projects_list/MainActivity.kt
@@ -4,17 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.github.first_non_interesting_username.projects_list.ui.theme.ProjectsTheme
+import androidx.compose.ui.Alignment
 
 
 class MainActivity : ComponentActivity() {
@@ -36,10 +36,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Cyan) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
         Text(
             text = "Hi, my name is $name!",
-            modifier = modifier.padding(24.dp)
         )
     }
 }
