34078aa4cbd2c288d47975294f32b673942b1b48
First-Non-Interesting-Username <janekmusin@proton.me>
2026-07-31 10:23:30 +0200

    start working on adding new project screen

--- app/src/main/java/io/github/first_non_interesting_username/projects_list/ui/screens/NewProjectScreen.kt
+++ app/src/main/java/io/github/first_non_interesting_username/projects_list/ui/screens/NewProjectScreen.kt
@@ -1,16 +1,24 @@
 package io.github.first_non_interesting_username.projects_list.ui.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -44,12 +52,26 @@ fun NewProjectScreen(navController: NavHostController) {
                 .padding(4.dp),
         ) {
             HorizontalDivider()
+            Spacer(Modifier.height(8.dp))
+            OutlinedTextField(
+                state = rememberTextFieldState(),
+                label = { Text("Name")},
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .fillMaxWidth(0.8f),
+            )
+            Spacer(Modifier.height(8.dp))
+            OutlinedTextField(
+                state = rememberTextFieldState(),
+                label = { Text("Description") },
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .fillMaxWidth(0.8f),
+                lineLimits = TextFieldLineLimits.MultiLine(
+                    minHeightInLines = 3,
+                    maxHeightInLines = 6
+                )
+            )
         }
     }
-}
-
-@Composable
-fun TextBox(
-    modifier: Modifier = Modifier,
-) {
 }
