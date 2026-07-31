a136f5b9c692d1a17bf5a77678633cc2cd65508c
First-Non-Interesting-Username <janekmusin@proton.me>
2026-07-31 15:59:39 +0200

    link row still unusable

--- app/src/main/java/io/github/first_non_interesting_username/projects_list/ui/screens/NewProjectScreen.kt
+++ app/src/main/java/io/github/first_non_interesting_username/projects_list/ui/screens/NewProjectScreen.kt
@@ -2,15 +2,19 @@ package io.github.first_non_interesting_username.projects_list.ui.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.input.TextFieldLineLimits
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -31,6 +35,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -179,24 +184,37 @@ fun LinkRow(
     onClick: () -> Unit,
 ) {
     Row(modifier = modifier.then(
-        Modifier.background(MaterialTheme.colorScheme.background)
+        Modifier
+            .height(64.dp)
+            .clip(RoundedCornerShape(4.dp))
+            //.background(MaterialTheme.colorScheme.onBackground)
     )) {
         Box(
             modifier = Modifier
                 .weight(1f)
         ) {
-            if (isEditing) {
-                OutlinedTextField(
-                    value = initialText,
-                    onValueChange = onValueChange,
-                )
-            } else {
-                Text(
-                    text = initialText,
-                    modifier = Modifier
-                        .clickable {
-                            onClick()
+            OutlinedTextField(
+                value = initialText,
+                onValueChange = onValueChange,
+                readOnly = !isEditing,
+                trailingIcon = {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_edit),
+                        contentDescription = "Edit",
+                        modifier = Modifier.clickable {
+                            onEditClick(true)
                         }
+                    )
+                }
+            )
+            if (!isEditing) {
+                Box(
+                    modifier = Modifier
+                        .matchParentSize()
+                        .clickable(
+                            interactionSource = remember { MutableInteractionSource() },
+                            indication = null,
+                        ) { onClick() }
                 )
             }
         }
