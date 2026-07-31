e11982b5c67c008d0d120c93e92e8327c1739fdd
First-Non-Interesting-Username <janekmusin@proton.me>
2026-07-31 11:48:05 +0200

    create sloppy sliders in the new project screen

--- app/src/main/java/io/github/first_non_interesting_username/projects_list/ui/screens/NewProjectScreen.kt
+++ app/src/main/java/io/github/first_non_interesting_username/projects_list/ui/screens/NewProjectScreen.kt
@@ -1,6 +1,8 @@
 package io.github.first_non_interesting_username.projects_list.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,20 +16,29 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import io.github.first_non_interesting_username.projects_list.R
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NewProjectScreen(navController: NavHostController) {
+    var priorityFloat by remember { mutableFloatStateOf(0f) }
+    var motivationFloat by remember { mutableFloatStateOf(0f) }
+    var isEditing by remember { mutableStateOf(false) }
+    var linkText by remember { mutableStateOf("") }
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -51,6 +62,7 @@ fun NewProjectScreen(navController: NavHostController) {
                 .padding(innerPadding)
                 .padding(4.dp),
         ) {
+
             HorizontalDivider()
             Spacer(Modifier.height(8.dp))
             OutlinedTextField(
@@ -72,6 +84,66 @@ fun NewProjectScreen(navController: NavHostController) {
                     maxHeightInLines = 6
                 )
             )
+            Spacer(Modifier.height(8.dp))
+            SliderWithDescription(
+                value = priorityFloat,
+                onValueChange = { priorityFloat = it },
+                text = "Priority",
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .fillMaxWidth(0.8f),
+                textModifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .fillMaxWidth(0.8f),
+                keyModifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .fillMaxWidth(0.8f),
+                )
+            Spacer(Modifier.height(8.dp))
+            SliderWithDescription(
+                value = motivationFloat,
+                onValueChange = { motivationFloat = it },
+                text = "Motivation",
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .fillMaxWidth(0.8f),
+                textModifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .fillMaxWidth(0.8f),
+                keyModifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .fillMaxWidth(0.8f),
+            )
+        }
+    }
+}
+
+@Composable
+fun SliderWithDescription(
+    value: Float,
+    modifier: Modifier,
+    onValueChange: (Float) -> Unit,
+    textModifier: Modifier,
+    text: String,
+    keyModifier: Modifier,
+) {
+
+    Text(text = text,
+        modifier = textModifier)
+    Slider(
+        value = value,
+        onValueChange = onValueChange,
+        valueRange = 0f..10f,
+        steps = 9,
+        modifier = modifier,
+            )
+
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = keyModifier,
+    ) {
+        for (i in 0..10) {
+            Text(text = i.toString())
         }
     }
 }
